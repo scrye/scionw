a0d1d79d91c8277cd5a961933270aae5cbb5a110
Sergiu Costea <sergiu.costea@gmail.com>
2017-05-12 03:02:15 +0300

    Initial commit

---
+++ dissector/packet-scion.c
@@ -0,0 +1,31 @@
+#include "config.h"
+
+#include <epan/packet.h>
+
+#define SCION_PORT 30096
+
+static int proto_scion = -1;
+
+void proto_register_scion(void) {
+	proto_scion = proto_register_protocol (
+		"SCION Protocol",
+		"SCION",
+		"scion"
+		);
+}
+
+void proto_reg_handoff_scion(void) {
+	static dissector_handle_t scion_handle;
+
+	scion_handle = create_dissector_handle(dissect_scion, proto_scion);
+	dissector_add_uint("udp.port", SCION_PORT, scion_handle);
+}
+
+static int dissect_scion(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree _U_, void *data _U_) {
+	col_set_ptr(pinfo->cinfo, COL_PROTOCOL, "SCION");
+
+	/* Clear out stuff in the info column */
+	col_clear(pinfo->cinfo, COL_INFO);
+
+	return tvb_captured_length(tvb);
+}
